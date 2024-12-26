cc66af1e85eb8eb98d1b1fe9419ec7aac36c6771
Sho <18667396+sho-kawano@users.noreply.github.com>
2024-12-26 04:32:45 -0800

    Updated paper materials

--- download_data.R
+++ download_data.R
@@ -2,7 +2,7 @@ source("sim_analysis_functions/load_county.R")
 key <- "" # PUT YOUR CENSUS API HERE
 home_folder <- paste0(getwd(), "/data") 
 
-# download NC only 
+# download NC data used for analysis/simulation study
 state <- "NC"
 data <- load_county(state, home_folder, key, save_data = T)
 
@@ -12,10 +12,11 @@ data_geom <- get_acs(geography = "county", variables = "B25071_001",
                year = 2019, state = state_abbrv, geometry = T, cb=F)
 saveRDS(data_geom, file="data/nc_data_w_geom.RDS")
 
-# download south atlantic region 
+# download south atlantic data used for analysis/simulation study
 region <- c("MD", "DE", "DC", "VA", "WV", "NC", "SC", "GA", "FL")
 data <- load_county(region, home_folder, key, save_data = T)
 
+# downloading the ACS data with geometry - used for plotting
 data_geom <- get_acs(geography = "county", variables = "B25071_001", 
                year = 2019, state = region, geometry = T, cb=F)
 saveRDS(data_geom, file="data/sa_data_w_geom.RDS")
